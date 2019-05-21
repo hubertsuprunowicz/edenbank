14d353c83eeead269fc4658ea3c1fb1206e02575
hubertsuprunowicz <42894408+hubertsuprunowicz@users.noreply.github.com>
2019-05-21 15:10:49 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$( "#counter" ).mouseover(function() {
+  
+    $('.counter').each(function() {
+          let $this = $(this),
+              countTo = $this.attr('data-count');
+
+          $({ countNum: $this.text()}).animate({
+            countNum: countTo
+          },{
+            duration: 3000,
+            easing:'linear',
+            step: function() {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function() {
+              $this.text(this.countNum);
+            }
+
+        });  
+    });   
+});
